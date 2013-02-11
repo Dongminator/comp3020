c04992751f257f544230a7bd5eba241b9bcca2fd
Dongminator <p.donglin@gmail.com>
2013-02-11 17:33:24 +0000

    removed testHeatmap function

--- static/googlemap.js
+++ static/googlemap.js
@@ -52,34 +52,8 @@ function addHeatmapLayer () {
 		heatmap.setMap(map);
 		
 	});
-	
 }
 
-function testHeatMap() {
-	/* Data points defined as an array of LatLng objects */
-	var heatmapData = [
-	  new google.maps.LatLng(37.782, -122.447),
-	  new google.maps.LatLng(37.782, -122.445),
-	  new google.maps.LatLng(37.782, -122.443),
-	  new google.maps.LatLng(37.782, -122.441),
-	  new google.maps.LatLng(37.782, -122.439),
-	  new google.maps.LatLng(37.782, -122.437),
-	  new google.maps.LatLng(37.782, -122.435),
-	  new google.maps.LatLng(37.785, -122.447),
-	  new google.maps.LatLng(37.785, -122.445),
-	  new google.maps.LatLng(37.785, -122.443),
-	  new google.maps.LatLng(37.785, -122.441),
-	  new google.maps.LatLng(37.785, -122.439),
-	  new google.maps.LatLng(37.785, -122.437),
-	  new google.maps.LatLng(37.785, -122.435)
-	];
-
-
-	var heatmap = new google.maps.visualization.HeatmapLayer({
-	  data: heatmapData
-	});
-	heatmap.setMap(map);
-}
 
 var start_place = null;
 var end_place = null;
