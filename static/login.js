19551a99a322302150995467970f98d28d35dcf7
Dongminator <p.donglin@gmail.com>
2013-03-26 16:46:37 +0000

    added recalculation of .infoContent div when resizing window.

--- static/login.js
+++ static/login.js
@@ -1,14 +1,15 @@
 var _ipinfodbAPIKey = "1902d940963f8a010cda2909819504d60670e7f129b8641eb0b620d1aac1f4c4";
+var _buttonHeight = 36;
+var _h2Height = 27;
 var loginButton;
 $(document).ready(function(){
 	ipToLocation();
 });
 
-function initialize(center) {
-	var zoomLevel = 8;
+function initialize(center, zoomLevel) {
 	if (!center) {
 		center = new google.maps.LatLng(0, 0);
-		zoomLevel = 2;
+		zoomLevel = 1;
 	}
 	var mapOptions = {
 			draggable: false,
@@ -16,30 +17,61 @@ function initialize(center) {
 			scrollwheel: false, 
 			disableDoubleClickZoom: true,
 			center: center,
-			zoom: 12,
+			zoom: zoomLevel,
 			mapTypeId: google.maps.MapTypeId.ROADMAP
 	};
 	var map = new google.maps.Map(document.getElementById("map"), mapOptions);
-	
 	contentSetup();
 }
 
 function ipToLocation () {
-	var url = "http://api.ipinfodb.com/v3/ip-city/?key=" + _ipinfodbAPIKey + "&format=json&callback=dataCallback";
-	try {
-		script = document.createElement('script');
-		script.src = url;
-		document.body.appendChild(script);
-	} catch(err) {}
-	
-	dataCallback = function(data){
-		lat = data.latitude;
-		lon = data.longitude;
-	    if (lat && lon) {
-	    	initialize(new google.maps.LatLng(lat, lon));
-	    }
-	};
+	if (navigator.geolocation) {
+		navigator.geolocation.getCurrentPosition(displayLocation, displayDefaultLocation);
+	} else {
+		initialize(new google.maps.LatLng(0, 0), 1);
+	}
 }
+
+function displayLocation (position) {
+	initialize(new google.maps.LatLng(position.coords.latitude, position.coords.longitude), 11);
+}
+
+function displayDefaultLocation (error) {
+	switch (error.code) {
+	case error.PERMISSION_DENIED:
+		initialize(new google.maps.LatLng(0, 0), 1);
+			break;
+	case error.POSITION_UNAVAILABLE:
+		initialize(new google.maps.LatLng(0, 0), 1);
+			break;
+	case error.TIMEOUT:
+		initialize(new google.maps.LatLng(0, 0), 1);
+			break;
+	case error.UNKNOWN_ERROR:
+		initialize(new google.maps.LatLng(0, 0), 1);
+			break;
+	}
+}
+
+/*
+ * Old version. The ipinfodb is not accurate because users of the website contribute to their database.
+ */
+//function ipToLocation () {
+//	var url = "http://api.ipinfodb.com/v3/ip-city/?key=" + _ipinfodbAPIKey + "&format=json&callback=dataCallback";
+//	try {
+//		script = document.createElement('script');
+//		script.src = url;
+//		document.body.appendChild(script);
+//	} catch(err) {}
+//	
+//	dataCallback = function(data){
+//		lat = data.latitude;
+//		lon = data.longitude;
+//	    if (lat && lon) {
+//	    	initialize(new google.maps.LatLng(lat, lon));
+//	    }
+//	};
+//}
 
 
 function contentSetup () {
@@ -49,11 +81,24 @@ function contentSetup () {
 		login();
     }).css('margin', '20px auto 10px auto').css('width', '350px');;
 
-	$('#leftInfo').css('float', 'left').css('margin', '0px 0px 0px 10px');
-	$('#rightInfo').css('float', 'right').css('margin', '0px 10px 0px 0px');
+	$('#leftInfo').css('float', 'left').css('margin', '0px 0px 0px 5px');
+	$('#rightInfo').css('float', 'right').css('margin', '0px 5px 0px 0px');
+	
+	resizeContent();
+}
+
+function resizeContent () {
+	var contentButtonH2Height = $('.infoContent').offset().top - $('#content').offset().top;
+	var contentHeight = $('#content').height();
 	
 	var newSize = $(window).height()*100/610;
-	$('p').css('font-size', newSize + "%");
+	if (newSize < 100) {
+		$('.infoContent').css('overflow-y', 'scroll').css('height', contentHeight - contentButtonH2Height);
+		$('p').css('font-size', '100%');
+	} else {
+		$('.infoContent').css('overflow-y', 'hidden').css('height', contentHeight - contentButtonH2Height);;
+		$('p').css('font-size', newSize + "%");
+	}
 }
 
 
@@ -109,7 +154,6 @@ function login() {
 
 
 $(window).resize(function() {
-	var newSize = $(window).height()*100/610;
-	$('p').css('font-size', newSize + "%");
+	resizeContent();
 });
 
